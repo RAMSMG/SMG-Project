36726ebee8bc696dcbe6734da4395043ad7133fb
sendmygift <sendmygift@sendmygifts-imac.local>
2016-07-28 16:48:33 +0530

    commit

--- productDetailsViewController.h
+++ productDetailsViewController.h
@@ -11,7 +11,13 @@
 #import <UIKit/UIKit.h>
 #import "KTImageView.h"
 @interface productDetailsViewController : UIViewController
+@property (nonatomic, strong) NSMutableArray *produts;
+@property (nonatomic, strong) NSString* selectedProductId;
 @property (nonatomic, assign) BOOL slideOutAnimationEnabled;
+
 @property (strong, nonatomic) IBOutlet UILabel *itemTitle;
 @property (strong, nonatomic) IBOutlet KTImageView *mainImage;
+@property (strong, nonatomic) IBOutlet KTImageView *smallImage;
+//@property (strong, nonatomic) IBOutlet UILabel *priceLabel;
+@property (strong, nonatomic) IBOutlet UILabel *priceLabel;
 @end
--- productDetailsViewController.m
+++ productDetailsViewController.m
@@ -10,14 +10,19 @@
 
 #import "productDetailsViewController.h"
 #import "SlideNavigationController.h"
+#define  url_fetch_product_id @"http://www.sendmygift.com/api/product_details.php?product_id";
 @interface productDetailsViewController ()
-
+{
+    NSArray *itemTitlearr,*mainImagearray,*smallImagesarray,*priceLabelarray;
+    
+}
 @end
 
 @implementation productDetailsViewController
-@synthesize slideOutAnimationEnabled;
+@synthesize slideOutAnimationEnabled,selectedProductId,produts;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
        // Do any additional setup after loading the view.
 }
 
@@ -35,14 +40,50 @@
     return YES;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)fetchCategoryData
+{
+    produts = [NSMutableArray new];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    config.timeoutIntervalForRequest = 120;
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    
+    NSString *urlStr = [NSString stringWithFormat:@"http://www.sendmygift.com/api/product_details.php?product_id=%@",self.selectedProductId];
+    
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            if (error)
+            {
+                NSLog(@"%@",error.localizedDescription);
+            }
+            else
+            {
+                NSError *err = nil;
+                
+                NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&err];
+                if (error) {
+                    NSLog(@"%@",error.description);
+                    
+                }else{
+                    NSLog(@"%@",jsonObject);
+                    
+                    itemTitlearr =[[jsonObject valueForKey:@"result"]valueForKey:@"name"];
+                    mainImagearray =[[jsonObject valueForKey:@"result"]valueForKey:@"pimg"];
+                    smallImagesarray=[[jsonObject valueForKey:@"result"]valueForKey:@"small_img"];
+                    priceLabelarray=[[jsonObject valueForKey:@"result"]valueForKey:@"price"];
+//                    titlearray=[[jsonObject valueForKey:@"result"]valueForKey:@"sub_cat_name"];
+                    
+                    NSLog(@"itemTitlearr %@,%lu",itemTitlearr,(unsigned long)itemTitlearr.count);
+                    NSLog(@"mainImagearray %@, %lu",mainImagearray,(unsigned long)mainImagearray.count);
+                    NSLog(@"smallImagesarray %@,%lu",smallImagesarray,(unsigned long)smallImagesarray.count);
+                    
+                }
+            }
+        });
+    }];
+    [task resume];
 }
-*/
 
 @end
