b000eb78d0163b4d3384172cacbe0b2dbb35aac0
sendmygift <sendmygift@sendmygifts-imac.local>
2016-07-28 12:54:31 +0530

    commit

--- productDetailsViewController.h
+++ productDetailsViewController.h
@@ -9,7 +9,9 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "KTImageView.h"
 @interface productDetailsViewController : UIViewController
 @property (nonatomic, assign) BOOL slideOutAnimationEnabled;
+@property (strong, nonatomic) IBOutlet UILabel *itemTitle;
+@property (strong, nonatomic) IBOutlet KTImageView *mainImage;
 @end
